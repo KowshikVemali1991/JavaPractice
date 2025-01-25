0031cb06272c4609fb5228841c6fda1633d15c54
KowshikVemali1991 <bapu.koushik@gmail.com>
2025-01-25 13:23:20 +0530

    Additions  Program

---
+++ Additions.java
@@ -0,0 +1,26 @@
+public class Additions {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c = a + b;
+        System.out.println(a+b);
+        System.out.println("***************");
+        System.out.println(c);
+        //addition of 10 and 20 is 30
+        System.out.println("Addition of " +a +" and " +b +" is " +c);
+
+        int a1=10, b1=20,c1=a1+b1;
+        System.out.println(a1+b1);
+        System.out.println(c1);
+
+        int a2=10,  b2=20, c2;
+
+        c2= a2+b2;
+        System.out.println(c2);
+        System.out.println("Addition of " +a +" and " +b +" is " +a+b);
+        System.out.println("Addition of " +a +" and " +b +" is " +(a+b));
+
+
+
+    }
+}
